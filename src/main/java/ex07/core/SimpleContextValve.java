fdfe6967f831f745405b708fc70e74ef17531d40
wangjy <wangjianyang111@163.com>
2024-08-28 16:50:33 +0800

    ex07 日志记录器

---
+++ src/main/java/ex07/core/SimpleContextValve.java
@@ -0,0 +1,78 @@
+package ex07.core;
+
+import org.apache.catalina.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class SimpleContextValve implements Valve, Contained {
+    protected Container container;
+
+    @Override
+    public void invoke(Request request, Response response, ValveContext valveContext) throws IOException, ServletException {
+        if (!(request.getRequest() instanceof HttpServletRequest)
+                || !(response.getResponse() instanceof HttpServletResponse)) {
+            return;
+        }
+
+        // Disallow any direct access to resources under WEB-INF or META-INF
+        HttpServletRequest httpServletRequest = (HttpServletRequest) request.getRequest();
+        String contextPath = httpServletRequest.getContextPath();
+        String requestUri = ((HttpRequest) request).getDecodedRequestURI();
+        String relativeUri = requestUri.substring(contextPath.length()).toUpperCase();
+
+        Context context = (Context) getContainer();
+        // Select the Wrapper to be used for this Request
+        Wrapper wrapper;
+        try {
+            wrapper = (Wrapper) context.map(request, true);
+        } catch (IllegalArgumentException e) {
+            badRequest(requestUri, (HttpServletResponse) response.getResponse());
+            return;
+        }
+        if (wrapper == null) {
+            notFound(requestUri, (HttpServletResponse) response.getResponse());
+            return;
+        }
+        // Ask this Wrapper to process this Request
+        response.setContext(context);
+        wrapper.invoke(request, response);
+    }
+
+    private void badRequest(String requestURI, HttpServletResponse response) {
+        try {
+            response.sendError(HttpServletResponse.SC_BAD_REQUEST, requestURI);
+        } catch (IllegalStateException e) {
+            ;
+        } catch (IOException e) {
+            ;
+        }
+    }
+
+    private void notFound(String requestURI, HttpServletResponse response) {
+        try {
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, requestURI);
+        } catch (IllegalStateException e) {
+            ;
+        } catch (IOException e) {
+            ;
+        }
+    }
+
+    @Override
+    public Container getContainer() {
+        return container;
+    }
+
+    @Override
+    public void setContainer(Container container) {
+        this.container = container;
+    }
+
+    @Override
+    public String getInfo() {
+        return null;
+    }
+}
---
+++ src/main/java/ex07/core/SimpleLoader.java
@@ -0,0 +1,124 @@
+package ex07.core;
+
+import org.apache.catalina.*;
+
+import java.beans.PropertyChangeListener;
+import java.io.File;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.net.URLStreamHandler;
+
+public class SimpleLoader implements Loader, Lifecycle {
+    public static final String WEB_ROOT = System.getProperty("user.dir")
+            + File.separator + "src"
+            + File.separator + "main"
+            + File.separator + "java"
+            + File.separator + "webroot";
+
+    private ClassLoader classLoader;
+    private Container container;
+
+    public SimpleLoader() {
+        try {
+            URL[] urls = new URL[1];
+            URLStreamHandler streamHandler = null;
+//            File classPath = new File(WEB_ROOT);
+//            String repository = (new URL("file", null, classPath.getCanonicalPath() + File.separator)).toString();
+//            urls[0] = new URL(null, repository, streamHandler);
+            urls[0] = new File(SimpleLoader.class.getResource("/webroot/").toURI()).toURI().toURL();
+            classLoader = new URLClassLoader(urls);
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public ClassLoader getClassLoader() {
+        return classLoader;
+    }
+
+    @Override
+    public Container getContainer() {
+        return container;
+    }
+
+    @Override
+    public void setContainer(Container container) {
+        this.container = container;
+    }
+
+    @Override
+    public boolean getDelegate() {
+        return false;
+    }
+
+    @Override
+    public void setDelegate(boolean b) {
+
+    }
+
+    @Override
+    public String getInfo() {
+        return null;
+    }
+
+    @Override
+    public boolean getReloadable() {
+        return false;
+    }
+
+    @Override
+    public void setReloadable(boolean b) {
+
+    }
+
+    @Override
+    public void addPropertyChangeListener(PropertyChangeListener propertyChangeListener) {
+
+    }
+
+    @Override
+    public void addRepository(String s) {
+
+    }
+
+    @Override
+    public String[] findRepositories() {
+        return new String[0];
+    }
+
+    @Override
+    public boolean modified() {
+        return false;
+    }
+
+    @Override
+    public void removePropertyChangeListener(PropertyChangeListener propertyChangeListener) {
+
+    }
+
+    @Override
+    public void addLifecycleListener(LifecycleListener lifecycleListener) {
+
+    }
+
+    @Override
+    public LifecycleListener[] findLifecycleListeners() {
+        return new LifecycleListener[0];
+    }
+
+    @Override
+    public void removeLifecycleListener(LifecycleListener lifecycleListener) {
+
+    }
+
+    @Override
+    public void start() throws LifecycleException {
+        System.out.println("Starting SimpleLoader");
+    }
+
+    @Override
+    public void stop() throws LifecycleException {
+
+    }
+}
